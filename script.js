c92a77ed4d8081fca0c9889858e62f9568828b57
Eric R. Howard <ehoward@hudsonhs.com>
2017-01-25 23:02:32 +0000

    update

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 // JavaScript File
 $(document).ready()
+$("submit").click(function(){
+    var y = $("#submit").val();
+    console.log("y");
+    
+  
+});
+var x =2
+console.log("x");
+
+
+//$("submit").click(function() {
+//        var blank = $("#input").val();
+    
+//  for (i=0 ; i < 5 ; i++) {
+//  $.getJSON(
+     
+//      "https://developers.google.com/maps/documentation/static-maps/intro",
+     
+        
+//     function(response) {
+       //  console.log(x);
+//        $("#results").append("<img src=" + response.data[0].images.fixed_width_downsampled.url + ">");
+//      });
+//     }
+//    });
+//});
